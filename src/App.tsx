79971dc172d6babda7ee37371b93a205347f9068
Ludvigpbf <ludvig.flyckt@gmail.com>
2024-01-13 01:36:40 +0100

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Is this working?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is working now maby now or now?{count}
+          count is working now maby now or now or?{count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
